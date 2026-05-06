8664356f324561062847b608c264e393a6f278ab
Thiago130126 <canalthiago750@gmail.com>
2026-05-06 17:08:39 -0300

    Roda adm coletor de lixo

--- routes/adm.js
+++ routes/adm.js
@@ -3,9 +3,15 @@ var router = express.Router();
 
 // Middlewares
 const authMidd = require('../middlewares/authMiddleware');
+const Books = require('../modules/books/bookModel');
+
+// Controller
+const bookControl = require('../modules/books/bookController');
 
 router.get('/', authMidd.admAuth, (req, res) => {
     res.render('adm/index', { title: 'Painel Administrativo' });
 });
+
+router.post('/limpar-lixo', authMidd.admAuth, bookControl.coletorLixoImagens);
 
 module.exports = router;
